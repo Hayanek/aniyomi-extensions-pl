3c69e1b442ff14e701a6be0a54d59f39a0fe3ca3
Hayanek <kontakt@wolfek.xyz>
2025-07-16 08:53:54 +0200

    change for test

--- .github/scripts/move-apks.py
+++ .github/scripts/move-apks.py
@@ -10,18 +10,7 @@ except FileNotFoundError:
 
 REPO_APK_DIR.mkdir(parents=True, exist_ok=True)
 
-apk_artifacts_dir = Path.home() / "apk-artifacts"
-print(f"Searching for APKs recursively in: {apk_artifacts_dir}")
-
-apks = list(apk_artifacts_dir.rglob("*.apk"))
-print(f"Found APK files: {[str(apk) for apk in apks]}")
-
-for apk in apks:
-    apk_name = apk.name.replace("-release.apk", ".apk")
-    print(f"Moving {apk} to {REPO_APK_DIR / apk_name}")
-    shutil.move(apk, REPO_APK_DIR / apk_name)
-
-for apk in (Path.home() / "apk-artifacts").rglob("*.apk"):
+for apk in (Path.home() / "apk-artifacts").glob("**/*.apk"):
     apk_name = apk.name.replace("-release.apk", ".apk")
 
     shutil.move(apk, REPO_APK_DIR / apk_name)
