4c0d6bae2f5c78f077f4d299accf27da7e6c1ecc
Hayanek <kontakt@wolfek.xyz>
2025-07-16 11:13:22 +0200

    fix

--- src/pl/ogladajanime/src/eu/kanade/tachiyomi/animeextension/pl/ogladajanime/OgladajAnime.kt
+++ src/pl/ogladajanime/src/eu/kanade/tachiyomi/animeextension/pl/ogladajanime/OgladajAnime.kt
@@ -23,9 +23,9 @@ import okhttp3.Response
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
-import uy.kohesive.injekt.injectLazy
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
+import uy.kohesive.injekt.injectLazy
 
 class OgladajAnime : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
 
