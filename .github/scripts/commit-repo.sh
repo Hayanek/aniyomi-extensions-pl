484f138dc85c241d215f6690f1157a621da1fa11
Hayanek <kontakt@wolfek.xyz>
2025-07-16 11:29:19 +0200

    fix

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 set -e
 
-rsync -a --delete --exclude .git --exclude .gitignore ../repo/ .
+rsync -a --delete --exclude .git --exclude .gitignore --exclude repo.json ../master/repo/ .
+git lfs install
 git config --global user.email "Janek-bot@users.noreply.github.com"
 git config --global user.name "Janek-bot"
 git status
 if [ -n "$(git status --porcelain)" ]; then
     git add .
-    git commit -S -m "Update extensions repo"
-    git push
-
-    # Purge cached index on jsDelivr
-    curl https://purge.jsdelivr.net/gh/aniyomiorg/aniyomi-extensions@repo/index.min.json
+    MSG="Update extensions repo"
+    if [[ $(git log -1 --pretty=format:%s) =~ "$MSG" ]]; then
+      git commit --amend --no-editQ
+      git push --force
+    else
+      git commit -m "$MSG"
+      git push
+    fi
 else
     echo "No changes to commit"
 fi
